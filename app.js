79d77b4fc9d07dabe2068ecf1b18bb7fe8b797c9
aryaman-applify <aryaman.j@applify.co>
2026-03-12 17:20:23 +0530

    pls work

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('IF you see this you have made a pipeline that automatically deploys latest docker image behind the lb and asg');
 });
-
+app.get('/',(req, res)=>{
+"pls work"});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
