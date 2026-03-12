b42464afb91e323a82e2514d0f91df2ab63f9ef5
aryaman-applify <aryaman.j@applify.co>
2026-03-12 16:18:52 +0530

    ye dil maange more

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CodePipeline + Docker!');
+  res.send('IF you see this you have made a pipeline that automatically deploys latest docker image behind the lb and asg');
 });
 
 const PORT = process.env.PORT || 3000;
